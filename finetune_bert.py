b1487e6bfd9630718d1902ffe67906025d5d07a8
Jack <jackhart0508@gmail.com>
2020-05-01 18:58:46 -0400

    tweaks to bert code

--- finetune_bert.py
+++ finetune_bert.py
@@ -13,9 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """BERT finetuning runner.
-
 @Jack -- Cleaned up script and added functionality for layer specific tuning.
-
 """
 
 from __future__ import absolute_import
